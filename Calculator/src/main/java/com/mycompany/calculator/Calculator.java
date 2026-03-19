801b44057ce16938ed7bdb0352b43d0ea8509b6b
AlexGit16Code <alexanderomombo@gmail.com>
2026-03-19 10:16:06 +0200

    Auto testing example

--- Calculator/src/main/java/com/mycompany/calculator/Calculator.java
+++ Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -22,5 +22,7 @@ public class Calculator {
     public double divide(double a, double b) {
         if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
         return a / b;
+        
+        //learning how to do auto unit testing
     }
 }
